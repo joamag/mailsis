48b96ea137ed155443cbeadb13bf34fee2695607
João Magalhães <joamag@gmail.com>
2025-04-15 19:28:15 +0100

    Bootstrap of the SMTP server (#1)
    Initial SMTP server implementation.

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Mailsis
 
-Simple (yet efficient) SMTP and IMAP server.
+Simple (yet efficient) [SMTP](https://datatracker.ietf.org/doc/html/rfc5321) and [IMAP](https://datatracker.ietf.org/doc/html/rfc3501) server.
+
+## Description
+
+Built on top of the powerful [Rust](https://www.rust-lang.org/) programming language, Mailsis provides a safe and efficient implementation of the classic SMTP and IMAP protocols.
+
+### Features
+
+- Simple and efficient SMTP server
+- Powerful IMAP server
+- Authentication with [SASL](https://datatracker.ietf.org/doc/html/rfc4422)
+- Mailbox storage, with support for multiple users
+- File-system based, allowing for easy integration with existing systems
+
+## License
+
+Mailsis is currently licensed under the [Apache License, Version 2.0](http://www.apache.org/licenses/).
---
+++ src/main.rs
@@ -0,0 +1,508 @@
+use base64::{engine::general_purpose, Engine as _};
+use chrono::Utc;
+use rustls::server::ServerSessionMemoryCache;
+use rustls_pemfile::{certs, pkcs8_private_keys};
+use std::{
+    collections::{HashMap, HashSet},
+    error::Error,
+    fs::File as StdFile,
+    io::BufReader as StdBufReader,
+    sync::Arc,
+};
+use tokio::{
+    fs::{self, File},
+    io::{
+        split, AsyncBufRead, AsyncBufReadExt, AsyncRead, AsyncWrite, AsyncWriteExt, BufReader,
+        ReadHalf, WriteHalf,
+    },
+    net::{TcpListener, TcpStream},
+    sync::mpsc,
+};
+use tokio_rustls::{
+    rustls::{Certificate, PrivateKey, ServerConfig},
+    TlsAcceptor, TlsStream,
+};
+use uuid::Uuid;
+
+struct SMTPSession {
+    from: String,
+    rcpts: HashSet<String>,
+    authenticated: bool,
+    auth_required: bool,
+    credentials: Arc<HashMap<String, String>>,
+}
+
+impl Default for SMTPSession {
+    fn default() -> Self {
+        Self {
+            from: String::new(),
+            rcpts: HashSet::new(),
+            authenticated: false,
+            auth_required: false,
+            credentials: Arc::new(HashMap::new()),
+        }
+    }
+}
+
+/// Stores the email in the mailbox directory, creates the directory if it doesn't exist.
+/// Each user has their own directory, and each email is stored in a file named with a UUID.
+///
+/// There's no limit to the number of emails that can be stored.
+async fn store_email(from: String, rcpt: String, body: String) -> Result<(), Box<dyn Error>> {
+    let safe_rcpt = rcpt.replace(|c: char| !c.is_ascii_alphanumeric(), "_");
+    let path = format!("mailbox/{}", safe_rcpt);
+    fs::create_dir_all(&path).await?;
+    let filename = format!("{}/{}.eml", path, Uuid::new_v4());
+    let mut file = File::create(&filename).await?;
+    if !is_mime_valid(&body).await {
+        file.write_all(format!("From: {}\r\n", from).as_bytes())
+            .await?;
+        file.write_all(format!("To: {}\r\n", rcpt).as_bytes())
+            .await?;
+        file.write_all(format!("Date: {}\r\n\r\n", Utc::now().to_rfc2822()).as_bytes())
+            .await?;
+    }
+    file.write_all(body.as_bytes()).await?;
+    println!("Stored: {}", filename);
+    Ok(())
+}
+
+/// Checks if the email is a valid MIME email.
+///
+/// A valid MIME email starts with a line that contains "MIME-Version:".
+async fn is_mime_valid(body: &str) -> bool {
+    let mut lines = body.lines();
+    let first_line = lines.next().unwrap_or("");
+    let mime_type = first_line.split_whitespace().next().unwrap_or("");
+    mime_type == "MIME-Version:"
+}
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 4)]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:2525").await?;
+    let tls_config = Arc::new(load_tls_config().unwrap());
+    let tls_acceptor = TlsAcceptor::from(tls_config);
+    let credentials = Arc::new(load_credentials("users.txt"));
+    let (tx, mut rx) = mpsc::channel::<(String, HashSet<String>, String)>(100);
+
+    // Spawn a task to handle the email storage, this is a long running
+    // task that will run until the program is terminated
+    tokio::spawn(async move {
+        while let Some((from, rcpts, body)) = rx.recv().await {
+            for rcpt in rcpts {
+                if let Err(error) = store_email(from.clone(), rcpt, body.clone()).await {
+                    println!("Error storing email: {}", error);
+                }
+            }
+        }
+    });
+
+    println!("Mailsis SMTP running on port 2525");
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let tls_acceptor = tls_acceptor.clone();
+        let tx = tx.clone();
+        let credentials = credentials.clone();
+        tokio::spawn(async move {
+            handle_smtp_session(stream, tls_acceptor, tx, credentials).await;
+        });
+    }
+}
+
+/// Loads the TLS configuration from the files and returns a ServerConfig.
+///
+/// The files should be structured as follows:
+/// cert.pem: The certificate file.
+/// key.pem: The private key file.
+fn load_tls_config() -> Result<ServerConfig, Box<dyn Error>> {
+    let cert_file = &mut StdBufReader::new(StdFile::open("cert.pem")?);
+    let key_file = &mut StdBufReader::new(StdFile::open("key.pem")?);
+
+    let cert_chain = certs(cert_file)
+        .unwrap()
+        .into_iter()
+        .map(Certificate)
+        .collect();
+
+    // Load the private key from the key file as PKCS8
+    let mut keys = pkcs8_private_keys(key_file)?;
+    let key = PrivateKey(keys.remove(0));
+
+    let mut config = ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth()
+        .with_single_cert(cert_chain, key)?;
+
+    // Allow multiple sessions per client, making it possible to
+    // re-use the same TLS connection for multiple SMTP sessions
+    config.session_storage = ServerSessionMemoryCache::new(256);
+    Ok(config)
+}
+
+/// Loads the credentials from the file and returns a HashMap of usernames and passwords.
+///
+/// The file should be formatted as follows:
+/// username:password
+/// username2:password2
+/// username3:password3
+/// ...
+fn load_credentials(path: &str) -> HashMap<String, String> {
+    let mut creds = HashMap::new();
+    if let Ok(content) = std::fs::read_to_string(path) {
+        for line in content.lines() {
+            if let Some((user, pass)) = line.split_once(':') {
+                creds.insert(user.trim().to_string(), pass.trim().to_string());
+            }
+        }
+    }
+    creds
+}
+
+impl SMTPSession {
+    /// Create a new SMTP session with default values.
+    pub fn new(credentials: Arc<HashMap<String, String>>, auth_required: bool) -> Self {
+        Self {
+            credentials,
+            auth_required,
+            ..Default::default()
+        }
+    }
+
+    /// Base handler for the SMTP commands, should concentrate all the
+    /// command handling in a single place for better maintainability.
+    pub async fn handle_base<R: AsyncRead + AsyncBufRead + Unpin, W: AsyncWrite + Unpin>(
+        &mut self,
+        reader: &mut R,
+        writer: &mut W,
+        tx: &mpsc::Sender<(String, HashSet<String>, String)>,
+        line: &mut String,
+        command: &String,
+        arg: Option<&str>,
+    ) {
+        match command.as_str() {
+            "EHLO" | "HELO" => {
+                self.handle_ehlo_helo(writer).await;
+            }
+            "AUTH" if arg == Some("LOGIN") => {
+                self.handle_auth_login(reader, writer, line).await;
+            }
+            "AUTH" => {
+                if let Some(arg) = arg {
+                    if let Some(encoded_user) = arg.strip_prefix("LOGIN") {
+                        self.handle_auth_with_username(reader, writer, line, encoded_user)
+                            .await;
+                    } else {
+                        writer
+                            .write_all(b"504 Unrecognized authentication type\r\n")
+                            .await
+                            .ok();
+                    }
+                } else {
+                    writer
+                        .write_all(b"504 Unrecognized authentication type\r\n")
+                        .await
+                        .ok();
+                }
+            }
+            "MAIL" => {
+                if let Some(value) = arg {
+                    self.handle_mail(writer, value).await;
+                }
+            }
+            "RCPT" => {
+                if let Some(value) = arg {
+                    self.handle_rcpt(writer, value).await;
+                }
+            }
+            "DATA" => {
+                self.handle_data(reader, writer, tx, line).await;
+            }
+            "QUIT" => {
+                self.handle_quit(writer).await;
+            }
+            _ => {
+                println!("Unknown command: {}", command);
+                self.handle_unknown(writer).await;
+            }
+        }
+    }
+
+    async fn handle_ehlo_helo<W: AsyncWrite + Unpin>(&self, writer: &mut W) {
+        writer.write_all(b"250-localhost greets you\r\n").await.ok();
+        writer.write_all(b"250-STARTTLS\r\n").await.ok();
+        writer.write_all(b"250 AUTH LOGIN\r\n").await.ok();
+    }
+
+    async fn handle_auth_login<R: AsyncRead + AsyncBufRead + Unpin, W: AsyncWrite + Unpin>(
+        &mut self,
+        reader: &mut R,
+        writer: &mut W,
+        line: &mut String,
+    ) {
+        writer.write_all(b"334 VXNlcm5hbWU6\r\n").await.ok();
+
+        line.clear();
+        reader.read_line(line).await.ok();
+        let username = general_purpose::STANDARD
+            .decode(line.trim())
+            .unwrap_or_default();
+        let username = String::from_utf8_lossy(&username);
+
+        writer.write_all(b"334 UGFzc3dvcmQ6\r\n").await.ok();
+
+        line.clear();
+        reader.read_line(line).await.ok();
+        let password = general_purpose::STANDARD
+            .decode(line.trim())
+            .unwrap_or_default();
+        let password = String::from_utf8_lossy(&password);
+
+        if self.credentials.get(username.trim()) == Some(&password.trim().to_string()) {
+            writer
+                .write_all(b"235 Authentication successful\r\n")
+                .await
+                .ok();
+            self.authenticated = true;
+        } else {
+            writer
+                .write_all(b"535 Authentication failed\r\n")
+                .await
+                .ok();
+        }
+    }
+
+    async fn handle_auth_with_username<
+        R: AsyncRead + AsyncBufRead + Unpin,
+        W: AsyncWrite + Unpin,
+    >(
+        &mut self,
+        reader: &mut R,
+        writer: &mut W,
+        line: &mut String,
+        encoded_user: &str,
+    ) {
+        let username = general_purpose::STANDARD
+            .decode(encoded_user.trim())
+            .unwrap_or_default();
+        let username = String::from_utf8_lossy(&username);
+
+        writer.write_all(b"334 UGFzc3dvcmQ6\r\n").await.ok();
+
+        line.clear();
+        reader.read_line(line).await.ok();
+        let password = general_purpose::STANDARD
+            .decode(line.trim())
+            .unwrap_or_default();
+        let password = String::from_utf8_lossy(&password);
+
+        if self.credentials.get(username.trim()) == Some(&password.trim().to_string()) {
+            writer
+                .write_all(b"235 Authentication successful\r\n")
+                .await
+                .ok();
+            self.authenticated = true;
+        } else {
+            writer
+                .write_all(b"535 Authentication failed\r\n")
+                .await
+                .ok();
+        }
+    }
+
+    async fn handle_mail<W: AsyncWrite + Unpin>(&mut self, writer: &mut W, value: &str) {
+        if !self.authenticated && self.auth_required {
+            writer
+                .write_all(b"530 Authentication required\r\n")
+                .await
+                .ok();
+            return;
+        }
+        if let Some(value) = value.strip_prefix("FROM:") {
+            println!("MAIL FROM: {}", value);
+            self.from = value.trim().to_string();
+            writer.write_all(b"250 OK\r\n").await.ok();
+        } else {
+            writer
+                .write_all(b"501 Syntax error in parameters or arguments\r\n")
+                .await
+                .ok();
+        }
+    }
+
+    async fn handle_rcpt<W: AsyncWrite + Unpin>(&mut self, writer: &mut W, value: &str) {
+        if !self.authenticated && self.auth_required {
+            writer
+                .write_all(b"530 Authentication required\r\n")
+                .await
+                .ok();
+            return;
+        }
+        if let Some(value) = value.strip_prefix("TO:") {
+            println!("RCPT TO: {}", value);
+            self.rcpts.insert(value.trim().to_string());
+            writer.write_all(b"250 OK\r\n").await.ok();
+        } else {
+            writer
+                .write_all(b"501 Syntax error in parameters or arguments\r\n")
+                .await
+                .ok();
+        }
+    }
+
+    async fn handle_data<R: AsyncRead + AsyncBufRead + Unpin, W: AsyncWrite + Unpin>(
+        &mut self,
+        reader: &mut R,
+        writer: &mut W,
+        tx: &mpsc::Sender<(String, HashSet<String>, String)>,
+        line: &mut String,
+    ) {
+        if !self.authenticated && self.auth_required {
+            writer
+                .write_all(b"530 Authentication required\r\n")
+                .await
+                .ok();
+            return;
+        }
+        if self.rcpts.is_empty() {
+            writer.write_all(b"554 No valid recipients\r\n").await.ok();
+            return;
+        }
+
+        writer
+            .write_all(b"354 End data with <CR><LF>.<CR><LF>\r\n")
+            .await
+            .ok();
+
+        let mut data = String::new();
+        loop {
+            line.clear();
+            reader.read_line(line).await.ok();
+            if line == ".\r\n" {
+                break;
+            }
+            data.push_str(line);
+        }
+
+        let _ = tx.send((self.from.clone(), self.rcpts.clone(), data)).await;
+        writer.write_all(b"250 Message accepted\r\n").await.ok();
+
+        self.from.clear();
+        self.rcpts.clear();
+    }
+
+    async fn handle_quit<W: AsyncWrite + Unpin>(&self, writer: &mut W) {
+        self.write(writer, 221, "Bye").await;
+    }
+
+    async fn handle_unknown<W: AsyncWrite + Unpin>(&self, writer: &mut W) {
+        self.write(writer, 502, "Command not implemented").await;
+    }
+
+    async fn write<W: AsyncWrite + Unpin>(&self, writer: &mut W, code: u16, message: &str) {
+        writer
+            .write_all(format!("{} {}\r\n", code, message).as_bytes())
+            .await
+            .ok();
+    }
+}
+
+async fn handle_loop(
+    reader: ReadHalf<TcpStream>,
+    mut writer: WriteHalf<TcpStream>,
+    tls_acceptor: TlsAcceptor,
+    tx: mpsc::Sender<(String, HashSet<String>, String)>,
+    session: &mut SMTPSession,
+) {
+    let mut line = String::new();
+    let mut reader = BufReader::new(reader);
+
+    let _ = writer.write_all(b"220 localhost SimpleSMTP\r\n").await;
+
+    loop {
+        line.clear();
+
+        let bytes = reader.read_line(&mut line).await.unwrap_or(0);
+        if bytes == 0 {
+            break;
+        }
+
+        let line_clone = line.clone();
+
+        let cmd = line_clone.trim_end();
+        let mut parts: std::str::SplitWhitespace<'_> = cmd.split_whitespace();
+        let command = parts.next().unwrap_or("").to_uppercase();
+        let arg = parts.next();
+
+        println!("> {}", cmd);
+
+        match command.as_str() {
+            "STARTTLS" => {
+                writer.write_all(b"220 Ready to start TLS\r\n").await.ok();
+                let stream = ReadHalf::unsplit(reader.into_inner(), writer);
+                match tls_acceptor.accept(stream).await {
+                    Ok(tls_stream) => {
+                        println!("TLS handshake complete");
+                        return handle_tls_session(TlsStream::Server(tls_stream), tx, session)
+                            .await;
+                    }
+                    Err(e) => {
+                        eprintln!("TLS handshake failed: {:?}", e);
+                        return;
+                    }
+                }
+            }
+            _ => {
+                session
+                    .handle_base(&mut reader, &mut writer, &tx, &mut line, &command, arg)
+                    .await;
+            }
+        }
+    }
+}
+
+async fn handle_smtp_session(
+    stream: TcpStream,
+    tls_acceptor: TlsAcceptor,
+    tx: mpsc::Sender<(String, HashSet<String>, String)>,
+    credentials: Arc<HashMap<String, String>>,
+) {
+    // Create a new SMTP session with default values, split the stream into reader and writer
+    // and handle the loop starting the SMTP session
+    let mut session = SMTPSession::new(credentials.clone(), false);
+    let (reader, writer) = split(stream);
+    handle_loop(reader, writer, tls_acceptor, tx, &mut session).await;
+}
+
+async fn handle_tls_session(
+    stream: TlsStream<TcpStream>,
+    tx: mpsc::Sender<(String, HashSet<String>, String)>,
+    session: &mut SMTPSession,
+) {
+    let (reader, mut writer) = tokio::io::split(stream);
+    let mut reader = BufReader::new(reader);
+    let mut line = String::new();
+
+    let _ = writer.write_all(b"220 TLS secured SMTP\r\n").await;
+
+    loop {
+        line.clear();
+        let bytes = reader.read_line(&mut line).await.unwrap_or(0);
+        if bytes == 0 {
+            break;
+        }
+
+        let line_clone = line.clone();
+
+        let cmd = &line_clone.trim_end();
+        println!("(TLS) > {}", cmd);
+
+        let mut parts = cmd.split_whitespace();
+        let command = parts.next().unwrap_or("").to_uppercase();
+        let arg = parts.next();
+
+        session
+            .handle_base(&mut reader, &mut writer, &tx, &mut line, &command, arg)
+            .await;
+    }
+}
