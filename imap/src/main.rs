8d4cb68d13b3054cf789c7cbd0e31e395ccf3fb3
João Magalhães <joamag@gmail.com>
2025-04-18 00:56:38 +0100

    feat: initial naive and working IMAP implementation

--- imap/src/main.rs
+++ imap/src/main.rs
@@ -1,4 +1,4 @@
-use mailsis_utils::get_crate_root;
+use mailsis_utils::{get_crate_root, uid_fetch_range_str};
 use std::{error::Error, path::PathBuf, str::FromStr};
 use tokio::{
     fs::{read_dir, read_to_string},
@@ -37,13 +37,22 @@ impl IMAPSession {
         match command {
             "LOGIN" => self.handle_login(writer, tag, parts).await,
             "LOGOUT" => self.handle_logout(writer, tag).await,
+            "LIST" => self.handle_list(writer, tag, parts).await,
             "SELECT" | "EXAMINE" => self.handle_select(writer, tag, parts).await,
             "SEARCH" => self.handle_search(writer, tag, parts).await,
             "FETCH" => self.handle_fetch(writer, tag, parts).await,
+            "STATUS" => self.handle_status(writer, tag, parts).await,
+            "UID" => self.handle_uid(writer, tag, parts).await,
             "CAPABILITY" => self.handle_capability(writer, tag).await,
+            "NOOP" => self.handle_noop(writer, tag).await,
             _ => {
-                self.write_response(writer, tag, "BAD", "Unknown command")
-                    .await
+                self.write_response(
+                    writer,
+                    tag,
+                    "BAD",
+                    format!("Unknown command ({})", command).as_str(),
+                )
+                .await
             }
         }
     }
@@ -56,7 +65,7 @@ impl IMAPSession {
     ) -> Result<(), Box<dyn Error>> {
         if parts.len() >= 4 {
             self.authenticated = true;
-            self.username = Some(parts[2].to_string());
+            self.username = Some(parts[2].to_string().trim_matches('"').to_string());
             self.write_response(writer, tag, "OK", "LOGIN completed")
                 .await?;
         } else {
@@ -79,6 +88,26 @@ impl IMAPSession {
         Ok(())
     }
 
+    async fn handle_list<W: AsyncWrite + Unpin>(
+        &mut self,
+        writer: &mut W,
+        tag: &str,
+        parts: &[&str],
+    ) -> Result<(), Box<dyn Error>> {
+        if parts.len() >= 3 {
+            if parts[3] == "\"*\"" {
+                self.write_response(writer, "*", "LIST", "(\\HasNoChildren) \"/\" \"INBOX\"")
+                    .await?;
+            }
+            self.write_response(writer, tag, "OK", "LIST completed")
+                .await?;
+        } else {
+            self.write_response(writer, tag, "BAD", "Invalid mailbox name")
+                .await?;
+        }
+        Ok(())
+    }
+
     async fn handle_select<W: AsyncWrite + Unpin>(
         &mut self,
         writer: &mut W,
@@ -87,6 +116,11 @@ impl IMAPSession {
     ) -> Result<(), Box<dyn Error>> {
         if parts.len() >= 2 {
             self.mailbox = Some(parts[1].to_string());
+            let messages = self.search_messages("").await?;
+            self.write_response(writer, "*", "FLAGS", "(\\Seen)")
+                .await?;
+            self.write_response(writer, "*", &messages.len().to_string(), "EXISTS")
+                .await?;
             self.write_response(writer, tag, "OK", "SELECT completed")
                 .await?;
         } else {
@@ -151,26 +185,118 @@ impl IMAPSession {
         Ok(())
     }
 
-    async fn search_messages(&self, criteria: &str) -> Result<Vec<String>, Box<dyn Error>> {
-        let mut messages = Vec::new();
-        let mut entries = read_dir(&self.mailbox_path()).await?;
-
-        while let Some(entry) = entries.next_entry().await? {
-            let entry_path = entry.path();
-            let path_str = entry_path.to_str().unwrap();
-            if path_str.ends_with(".eml") {
-                let file_name = entry_path.file_name().unwrap().to_str().unwrap();
-                messages.push(file_name.to_string());
-            }
+    async fn handle_status<W: AsyncWrite + Unpin>(
+        &mut self,
+        writer: &mut W,
+        tag: &str,
+        parts: &[&str],
+    ) -> Result<(), Box<dyn Error>> {
+        if parts.len() >= 2 {
+            let mailbox = parts[1].to_string();
+            let messages = self.search_messages("").await?;
+            self.write_response(
+                writer,
+                "*",
+                "STATUS",
+                &format!(
+                    "\"{}\" (UIDNEXT {} MESSAGES {} UNSEEN 0 RECENT 0)",
+                    messages.len() + 1,
+                    messages.len(),
+                    mailbox
+                ),
+            )
+            .await?;
+            self.write_response(writer, tag, "OK", "STATUS completed")
+                .await?;
+        } else {
+            self.write_response(writer, tag, "BAD", "Invalid mailbox name")
+                .await?;
         }
 
-        Ok(messages)
+        Ok(())
     }
 
-    async fn fetch_message(&self, message_id: &str) -> Result<String, Box<dyn Error>> {
-        let path = self.mailbox_path().join(message_id);
-        let content = read_to_string(path).await?;
-        Ok(content)
+    async fn handle_uid<W: AsyncWrite + Unpin>(
+        &mut self,
+        writer: &mut W,
+        tag: &str,
+        parts: &[&str],
+    ) -> Result<(), Box<dyn Error>> {
+        let uid_command = parts[2].to_uppercase();
+        match uid_command.as_str() {
+            "FETCH" => self.handle_uid_fetch(writer, tag, parts).await?,
+            _ => {
+                self.write_response(
+                    writer,
+                    tag,
+                    "BAD",
+                    &format!("Invalid UID command {}", uid_command),
+                )
+                .await?;
+            }
+        }
+        Ok(())
+    }
+
+    async fn handle_uid_fetch<W: AsyncWrite + Unpin>(
+        &mut self,
+        writer: &mut W,
+        tag: &str,
+        parts: &[&str],
+    ) -> Result<(), Box<dyn Error>> {
+        let messages = self.search_messages("").await?;
+        let range = uid_fetch_range_str(&parts[3].to_string(), messages.len() as u32)
+            .ok_or("Invalid range")?;
+        let (start, end) = ((*range.start() - 1) as usize, (*range.end() - 1) as usize);
+        let messages_range = messages[start..=end].to_vec();
+
+        let message_indices: std::collections::HashMap<_, _> = messages
+            .iter()
+            .enumerate()
+            .map(|(idx, msg)| (msg, start + idx))
+            .collect();
+
+        for message in messages_range {
+            let index = message_indices[&message];
+            let contents = self.fetch_message(&message).await.unwrap();
+            let slices = parts[4..parts.len()]
+                .iter()
+                .map(|p| {
+                    let c = p.to_string();
+                    let a = c.trim_matches('(').trim_matches(')');
+                    match a {
+                        "FLAGS" => return "FLAGS (\\Unseen)".to_string(),
+                        "RFC822.SIZE" => return format!("RFC822.SIZE {}", message.len()),
+                        "BODY.PEEK[HEADER.FIELDS" => {
+                            return format!(
+                                "BODY[HEADER.FIELDS (To From Subject)] {{{}}}\r\n{}",
+                                contents.len(),
+                                contents
+                            )
+                        }
+                        "BODY[]" => {
+                            return format!("BODY[] {{{}}}\r\n{}", contents.len(), contents)
+                        }
+                        _ => "".to_string(),
+                    }
+                })
+                .collect::<Vec<String>>()
+                .iter()
+                .filter(|s| !s.is_empty())
+                .map(|s| s.to_string())
+                .collect::<Vec<String>>()
+                .join(" ");
+            self.write_response(
+                writer,
+                "*",
+                &(index + 1).to_string(),
+                format!("FETCH (UID {} {})", index + 1, slices).as_str(),
+            )
+            .await?;
+        }
+        self.write_response(writer, tag, "OK", "UID FETCH completed")
+            .await?;
+        Ok(())
     }
 
     async fn handle_capability<W: AsyncWrite + Unpin>(
@@ -183,6 +309,38 @@ impl IMAPSession {
         self.write_response(writer, tag, "OK", "CAPABILITY completed")
             .await?;
         Ok(())
+    }
+
+    async fn handle_noop<W: AsyncWrite + Unpin>(
+        &mut self,
+        writer: &mut W,
+        tag: &str,
+    ) -> Result<(), Box<dyn Error>> {
+        self.write_response(writer, tag, "OK", "NOOP completed")
+            .await?;
+        Ok(())
+    }
+
+    async fn search_messages(&self, criteria: &str) -> Result<Vec<String>, Box<dyn Error>> {
+        let mut messages = Vec::new();
+        let mut entries = read_dir(&self.mailbox_path()).await?;
+
+        while let Some(entry) = entries.next_entry().await? {
+            let entry_path = entry.path();
+            let path_str = entry_path.to_str().unwrap();
+            if path_str.ends_with(".eml") {
+                let file_stem = entry_path.file_stem().unwrap().to_str().unwrap();
+                messages.push(file_stem.to_string());
+            }
+        }
+
+        Ok(messages)
+    }
+
+    async fn fetch_message(&self, message_id: &str) -> Result<String, Box<dyn Error>> {
+        let path = self.mailbox_path().join(format!("{}.eml", message_id));
+        let content = read_to_string(path).await?;
+        Ok(content)
     }
 
     async fn write_raw<W: AsyncWrite + Unpin>(&self, writer: &mut W, data: &str) {
